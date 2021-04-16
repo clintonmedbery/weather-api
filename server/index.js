617d7eca57e6a9eba05ac45058c2ad0c7aedfc42
Clinton Medbery <clintomed@gmail.com>
2021-04-16 09:19:30 -0400

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { getWeatherByZipCode } from './controllers/weather.controller.js'
 import './loadEnv.js'
+import cors from 'cors'
 import { zipCodeValidation } from './middleware/validation/input.validation.js'
 const app = express()
 
 const port = process.env.PORT || 4000
 
+app.use(cors())
 app.get('/canary', (req, res) => {
   res.send('alive')
 })
